adc1dc22a862e183ccd3cf2d7842cd488500c7b5
stefano <stefano.pintore@ingv.it>
2020-12-11 18:35:48 +0100

    Start

---
+++ TEST/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+import requests
+import pytest
+import conftest
+
+
+@pytest.mark.parametrize(
+    "url,expected",
+    [
+        ("level=station&net=MN&format=xml&nodata=404",200),
+        ("level=network&net=MN&format=xml&nodata=404",200),
+        ("level=channelk&net=MN&format=xml&nodata=404",200),
+        ("level=response&net=MN&format=xml&nodata=404",200),
+    ],
+)
+
+def test_eval(url, expected,host):
+    response = requests.get( "http://"+host+":8080/exist/apps/fdsn-station/fdsnws/station/1/query/?" + url)
+    print (response.status_code)
+    assert response.status_code == expected 
+
+
+
