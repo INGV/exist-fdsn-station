00e2d64e2f5ba4ac4ac60df5e6fedc961a69d580
stefano <stefano.pintore@ingv.it>
2020-12-13 21:29:30 +0100

    More testing

--- TEST/conftest.py
+++ TEST/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--host", action="store", default="localhost", help="Exist fdsn host"
+        "--host", action="store", default="localhost:8080", help="Exist fdsn host"
     )
 
 
